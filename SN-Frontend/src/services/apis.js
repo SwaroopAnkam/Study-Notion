a7b9f52a9b4d716c9adf97bae39168cab42df930
Swaroop Ankam <swaroopankam04@gmail.com>
2023-09-27 15:34:41 +0530

    Added Navbar, State Management Files and Connected Frontend to Backend

---
+++ SN-Frontend/src/services/apis.js
@@ -0,0 +1,7 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+// CATAGORIES API
+export const categories = {
+  CATEGORIES_API: BASE_URL + "/course/showAllCategories",
+}
+
---
+++ SN-Frontend/src/slices/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  signupData: null,
+  loading: false,
+  token: localStorage.getItem("token")
+    ? JSON.parse(localStorage.getItem("token"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setToken(state, value) {
+      state.token = value.payload;
+    },
+  },
+});
+
+export const { setToken } = authSlice.actions;
+
+export default authSlice.reducer;
