cf8fa1b6f254f9b76ce45d4f95c89a714c0cb3c0
Swaroop Ankam <swaroopankam04@gmail.com>
2023-10-19 00:50:35 +0530

    Modified CORS poicy

--- SN-Backend/server.js
+++ SN-Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://studynotion-frontend-two.vercel.app/",
+    origin: "https://studynotion-frontend-two.vercel.app",
     credentials: true,
   })
 );
